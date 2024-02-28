53a8f367187a3f17b4cff36ce21e3b5f0bc08322
Priya Sandpa <21154@students.mrgs.school.nz>
2024-02-28 14:44:06 +1300

    Edited button clicker app to make it count how many times I clicked it.

--- GUI/Button Clicker/GUI-Button.py
+++ GUI/Button Clicker/GUI-Button.py
@@ -1,15 +1,21 @@
 from tkinter import *
 
-count=0
+count = 0
+
 def btn_click():
-    count=count+1
-    print("Button clicked ", count ," times.")
+    global count
+    count = count+1
+    count_text.config(text=count)
   
 
 window = Tk()
 
-btn = Button(window, text="Click Me!", command=btn_click)
 
+count_text = Label(text="0")
+count_text.pack()
+
+
+btn = Button(window, text="Click Me!", command=btn_click)
 btn.pack()
 
 window.mainloop()
