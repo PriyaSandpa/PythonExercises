585e49412261cee1b066ce71981c0be38f4b129c
Priya Sandpa <21154@students.mrgs.school.nz>
2024-03-08 12:10:30 +1300

    Made program that can show grade of a student, made class for "Student" and used [] and x.append to make list where I'll put the name of students in that list.

--- GUI/Class Grades Program/class_grade_program.py
+++ GUI/Class Grades Program/class_grade_program.py
@@ -1,14 +1,30 @@
 from tkinter import*
 
+class Student:
+    def __init__(self,name):
+        self.name=name
 
+    def get_grade(self):
+        return self.grade
     
+    def set_grade(self,grade):
+        self.grade=grade
+
+def show_grade():
+    grade_label.config(text=csc_2[0].get_grade())
+    
+csc_2=[]
+
+csc_2.append(Student("Boaz"))
+csc_2[0].set_grade("Achieved")
 
 window = Tk()
-window.geometry("300x400")
+window.geometry("300x300")
 window.title("Class Grades")
 
-label = Label(text="Enter the student's name: ")
-label.pack()
-input_box = Entry(text=0)
-input_box.pack()
+grade_label = Label()
+grade_label.pack()
+show_grade_btn = Button(text="Show Grade", command=show_grade)
+show_grade_btn.pack()
 
+window.mainloop()
