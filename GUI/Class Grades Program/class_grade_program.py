ea4b9093d1cce557939fc6e2a3c0dca94840ed86
Priya Sandpa <21154@students.mrgs.school.nz>
2024-03-07 18:31:11 +1300

    Made "Class Grades" program: - Coded for window, sized that window. - Made label for the window. - Made an entry box for the user to type in the student's name.

--- GUI/Class Grades Program/class_grade_program.py
+++ GUI/Class Grades Program/class_grade_program.py
@@ -1,3 +1,14 @@
+from tkinter import*
 
-        
+
     
+
+window = Tk()
+window.geometry("300x400")
+window.title("Class Grades")
+
+label = Label(text="Enter the student's name: ")
+label.pack()
+input_box = Entry(text=0)
+input_box.pack()
+
